89ec3e8c3e92c561fe68e7659fa5061afe62c5de
saintAlmond <ialmen1988@gmail.com>
2021-06-30 14:52:46 +0200

    lesson webpack

--- 04/src/script.js
+++ 04/src/script.js
@@ -1,1 +1,37 @@
 import './style.css'
+import * as THREE from 'three'
+
+//console.log(THREE)
+
+// scene
+const scene = new THREE.Scene()
+
+// cube
+const cube = new THREE.BoxGeometry(1,1,1)
+const material = new THREE.MeshBasicMaterial({color: 'red'})
+// a mesh is made of geometry and material
+const mesh = new THREE.Mesh(cube,material)
+
+// add mesh to the scene
+scene.add(mesh)
+
+// sizes
+const sizes = {
+    width: 800,
+    height: 600
+}
+
+// camera
+const camera = new THREE.PerspectiveCamera(75,sizes.width/sizes.height)
+camera.position.z=3
+scene.add(camera)
+
+// renderer
+const canvas = document.querySelector('.webgl')
+
+const renderer = new THREE.WebGLRenderer({
+    canvas : canvas
+})
+renderer.setSize(sizes.width,sizes.height)
+
+renderer.render(scene,camera)
