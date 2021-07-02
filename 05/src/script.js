294b4a8e8afccfe2f6ff44ba4ca67e8c2a5c2adf
saintAlmond <ialmen1988@gmail.com>
2021-07-02 21:34:51 +0200

    transform

--- 05/src/script.js
+++ 05/src/script.js
@@ -10,10 +10,43 @@ const scene = new THREE.Scene()
 /**
  * Objects
  */
+
+// axis helper
+const axis = new THREE.AxesHelper(2)
+scene.add(axis)
+
+const group = new THREE.Group()
+scene.add (group)
+
+
 const geometry = new THREE.BoxGeometry(1, 1, 1)
 const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
 const mesh = new THREE.Mesh(geometry, material)
-scene.add(mesh)
+group.add(mesh)
+//mesh.position.y=2
+mesh.position.set(1,1,1)
+mesh.position.normalize()
+console.log('origin to cube', mesh.position.length())
+
+mesh.scale.x = 0.5
+
+mesh.rotation.reorder('YXZ')
+mesh.rotation.x = 1 // radians
+mesh.rotation.y =1
+
+const mesh2 = new THREE.Mesh(
+    new THREE.BoxGeometry(1,1,1),
+    new THREE.MeshBasicMaterial({color: 'blue'})
+)
+
+group.add(mesh2)
+
+const mesh3 = new THREE.Mesh(
+    new THREE.BoxGeometry(1,1,1),
+    new THREE.MeshBasicMaterial({color: 'green'})
+)
+mesh3.position.x=-1
+group.add(mesh3)
 
 /**
  * Sizes
@@ -29,6 +62,10 @@ const sizes = {
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
 camera.position.z = 3
 scene.add(camera)
+camera.position.x = 1
+camera.lookAt(mesh.position)
+
+console.log('cube to camera',mesh.position.distanceTo(camera.position))
 
 /**
  * Renderer
