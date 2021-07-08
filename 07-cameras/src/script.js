9f27ea4bd7fe9535f8a79ca689cc0d081d192987
Fran <hola@beachlab.org>
2021-07-08 18:25:08 +0200

    orbitcontrol

--- 07-cameras/src/script.js
+++ 07-cameras/src/script.js
@@ -1,5 +1,6 @@
 import './style.css'
 import * as THREE from 'three'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 
 // cursor
 const cursor = {
@@ -39,8 +40,11 @@ const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 
 // const camera = new THREE.OrthographicCamera(-1 * aspectRatio, 1 * aspectRatio, 1, -1, 0.1, 100)
 // camera.position.x = 2
 // camera.position.y = 2
-// camera.position.z = 2
+camera.position.z = 2
 scene.add(camera)
+
+// controls
+const control = new OrbitControls(camera, canvas)
 
 // Renderer
 const renderer = new THREE.WebGLRenderer({
@@ -58,10 +62,10 @@ const tick = () => {
     // mesh.rotation.y = elapsedTime;
 
     // update camera
-    camera.position.x = Math.sin(cursor.x * Math.PI * 2) * 3
-    camera.position.z = Math.cos(cursor.x * Math.PI * 2) * 3
-    camera.position.y = cursor.y * 5
-    camera.lookAt(mesh.position)
+    // camera.position.x = Math.sin(cursor.x * Math.PI * 2) * 3
+    // camera.position.z = Math.cos(cursor.x * Math.PI * 2) * 3
+    // camera.position.y = cursor.y * 5
+    // camera.lookAt(mesh.position)
 
 
     // Render
