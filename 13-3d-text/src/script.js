44a289e9f9f0ba481c55b408689b701c697ab3bd
Fran <hola@beachlab.org>
2021-07-11 21:52:02 +0200

    with donuts

--- 13-3d-text/src/script.js
+++ 13-3d-text/src/script.js
@@ -59,6 +59,20 @@ fontLoader.load(
     }
 )
 
+// donuts
+for (var i = 0; i < 100; i++) {
+    const donutGeometry = new THREE.TorusGeometry(0.3, 0.2, 20, 45)
+    const donutMaterial = new THREE.MeshMatcapMaterial()
+    donutMaterial.matcap = matcapTexture
+    const donut = new THREE.Mesh(donutGeometry, donutMaterial)
+    donut.position.x = (Math.random() - 0.5) * 25
+    donut.position.y = (Math.random() - 0.5) * 25
+    donut.position.z = (Math.random() - 0.5) * 25
+    donut.rotation.x = Math.random() * Math.PI
+    donut.rotation.y = Math.random() * Math.PI
+    scene.add(donut)
+}
+
 // axis helper
 const axisHelper = new THREE.AxesHelper()
 scene.add(axisHelper)
